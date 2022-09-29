6a78a9a3405ca80bb91a6755e7ff1ea1a817e3ce
Nicholas <blueretrograde@gmail.com>
2022-09-29 17:20:35 +0100

    fixed some HTML language & linked my classes to consts on JS

---
+++ app.js
@@ -0,0 +1,83 @@
+//acessing the main display (where all the calculations will display)
+
+const mainDisplay = document.querySelector(".main__display")
+console.log("testing", mainDisplay)
+mainDisplay.innerText = "Test"
+//accessing the C (Delete) button
+
+const del = document.querySelector(".main__delete")
+
+//accessing the +/- (Positive to Negative button)
+
+const posToNeg = document.querySelector(".main__posToNeg")
+
+//acessing the % (Percentage) button
+
+const percentage = document.querySelector(".main__percentage")
+
+//acessing the / (Divide Button)
+
+const divide = document.querySelector(".main__divide")
+
+//acessing the number 9 button
+
+const num9 =  document.querySelector(".main__9")
+
+//acessing the number 8 button
+
+const num8 = document.querySelector(".main__8")
+
+//acessing the number 7 button
+
+const num7 = document.querySelector(".main__7")
+
+//acessing the X (Multiply Button)
+
+const multiply = document.querySelector("main__multiply")
+
+//acessing the number 6 button
+
+const num6 = document.querySelector(".main__6")
+
+//acessing the number 5 button
+
+const num5 = document.querySelector(".main__5")
+
+//acessing the number 4 button
+
+const num4 = document.querySelector(".main__4")
+
+//acessing the - (Subtract button)
+
+const subtract = document.querySelector(".main__subtract")
+
+////acessing the number 3 button
+
+const num3 = document.querySelector(".main__3")
+
+//acessing the number 2 button
+
+const num2 = document.querySelector(".main__2")
+
+//acessing the number 1 button
+
+const num1 = document.querySelector(".main__1")
+
+//acessing the + (Add button)
+
+const add = document.querySelector(".main__add")
+
+//acessing the number 0 button
+
+const num0 = document.querySelector(".main__0")
+
+//acessing the . (Decimal button)
+
+const decimal = document.querySelector(".main__decimal")
+
+//acessing the = (Equals button)
+
+const equals = document.querySelector(".equals")
+
+
+
