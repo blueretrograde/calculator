5fdc89e00721a5dda7283d772975e5977af91b16
Nicholas <blueretrograde@gmail.com>
2022-09-30 16:59:06 +0100

    changed some language & created a Switch statement for calc operations

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 
-//accessing the number buttons
-const numBtn = document.querySelectorAll(".data__number")
+//accesing the body of the calculator (including display)
 
-//accessing the operators (calculator functions)
-const operBtn = document.querySelectorAll(".data__operator")
+const calculator = document.querySelector(".calculator")
+
+//accessing all the keys on the calculator
+
+const keys = document.querySelector(".calculator__keys") 
+
+//acessing the display of the calculator
+
+const display = document.querySelector(".calculator__display")
 
 //accessing the equals button
 const equalsBtn = document.querySelector(".data__operator--equals")
@@ -11,3 +17,27 @@ const equalsBtn = document.querySelector(".data__operator--equals")
 //acessing the delete button
 const delBtn= document.querySelector(".data__operator--delete")
 
+//switch case for calculator functions (??????)
+
+switch() {
+    case '+':
+        result = number1 + number2;
+        console.log(number1 + " + " + number2 + " = " + result);
+        break;
+
+    case '-':
+        result = number1 - number2;
+        console.log(number1 + " - " + number2 + " = " + result);
+        break;
+
+    case '*':
+        result = number1 * number2;
+        console.log(number1 + " x " + number2 + " = " + result);
+        break;
+
+    case '/':
+        result = number1 / number2;
+        console.log(number1 + " / " + number2 + " = " + result);
+        break;
+
+//function for the operations (???)
